b3bcce6e2de5e6c2fecce261e0559ec5fea3233b
William Mumby <williammumby@Williams-MacBook-Pro.local>
2020-12-13 17:06:06 +0000

    Added navbar and api container, using mat blazor

--- src/Mumbdo.Web/Startup.cs
+++ src/Mumbdo.Web/Startup.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using MatBlazor;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Mumbdo.Web.Data;
 
 namespace Mumbdo.Web
 {
@@ -28,7 +22,7 @@ namespace Mumbdo.Web
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
+            services.AddMatBlazor();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

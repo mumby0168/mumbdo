ac930f332f38f18e9e747787017aa3b7fc17ba39
William Mumby <williammumby@Williams-MacBook-Pro.local>
2020-12-23 20:36:10 +0000

    added error logging

--- src/Mumbdo.Api/ExceptionHandlerMiddleware.cs
+++ src/Mumbdo.Api/ExceptionHandlerMiddleware.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Net;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 using Mumbdo.Domain.Exceptions;
 using Mumbdo.Shared;
 using Mumbdo.Shared.Dtos;
@@ -11,6 +13,14 @@ namespace Mumbdo.Api
 {
     public class ExceptionHandlerMiddleware : IMiddleware
     {
+        private readonly ILogger<ExceptionHandlerMiddleware> _logger;
+
+        public ExceptionHandlerMiddleware(ILogger<ExceptionHandlerMiddleware> logger)
+        {
+            _logger = logger;
+        }
+        
+        
         public async Task InvokeAsync(HttpContext context, RequestDelegate next)
         {
             try
@@ -19,13 +29,21 @@ namespace Mumbdo.Api
             }
             catch (ApplicationException e)
             {
+                _logger.LogError("Application exception thrown", e);
                 context.Response.StatusCode = (int) e.StatusCode;
                 await context.Response.WriteAsJsonAsync(new MumbdoErrorDto(e.ErrorCode, e.Message));
             }
             catch (DomainException e)
             {
+                _logger.LogError("Domain exception thrown", e);
                 context.Response.StatusCode = (int) HttpStatusCode.BadRequest;
                 await context.Response.WriteAsJsonAsync(new MumbdoErrorDto(e.ErrorCode, e.Message));
+            }
+            catch (Exception e)
+            {
+                _logger.LogError("Unhandled error", e);
+                context.Response.StatusCode = (int) HttpStatusCode.InternalServerError;
+                await context.Response.WriteAsync(e.ToString());
             }
             
         }
